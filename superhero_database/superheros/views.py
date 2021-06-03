bb4d5bcd36baefceedd558fa9c4fd26d8e774130
Your Name <you@example.com>
2021-06-02 21:20:59 -0500

    template now displays superhero names

--- superhero_database/superheros/views.py
+++ superhero_database/superheros/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Superhero
+
 
 # Create your views here.
+
+def index(request):
+    all_superheros = Superhero.objects.all()
+    context = {
+        'all_superheros': all_superheros
+    }
+    return render(request, 'superheros/index.html', context)
